ab3fe1fece3c687e2316b066a3b168d9e5e63c78
Jacob Day <jacob.m.day@icloud.com>
2019-03-26 11:43:36 -0500

    Update fade.js

--- fade.js
+++ fade.js
@@ -1,1 +1,17 @@
+// Fades Heading
+     (function() {
 
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2500)
+            .delay(5000)
+            .fadeOut(2500, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
